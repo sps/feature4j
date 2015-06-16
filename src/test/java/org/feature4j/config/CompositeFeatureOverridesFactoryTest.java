dcef8860f0ee2ba91e0d20f193d9d0ce4ec37bfc
Dann Webster <dannwebster@livenation.com>
2015-06-15 19:22:37 -0700

    Added License headers to all new files; removed unnecessary comments

--- src/test/java/org/feature4j/config/CompositeFeatureOverridesFactoryTest.java
+++ src/test/java/org/feature4j/config/CompositeFeatureOverridesFactoryTest.java
@@ -1,13 +1,30 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.feature4j.config;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import junit.framework.TestCase;
 import org.feature4j.FeatureOverride;
-import org.feature4j.MatcherFeatureOverride;
 import org.junit.Test;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by dannwebster on 6/15/15.
